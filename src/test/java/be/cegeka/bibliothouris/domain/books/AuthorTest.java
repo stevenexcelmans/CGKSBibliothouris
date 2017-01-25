8cd69303c083e355473cd54a0ef3ff19a0aec8bf
Sophie Mullen <sophie.mullen@cegeka.com>
2017-01-25 13:06:13 +0100

    test authoer equal

--- src/test/java/be/cegeka/bibliothouris/domain/books/AuthorTest.java
+++ src/test/java/be/cegeka/bibliothouris/domain/books/AuthorTest.java
@@ -11,14 +11,21 @@ public class AuthorTest {
     private Author author;
 
     @Before
-    public void setup(){
+    public void setup() {
         author = new Author("Dan", "Brown");
     }
 
     @Test
-    public void toString_returnsString () throws Exception {
+    public void toString_returnsString() throws Exception {
         String expected = "Dan Brown";
         Assertions.assertThat(author.toString()).isEqualTo(expected);
+    }
+
+    @Test
+    public void equals_returnsEqualObject() throws Exception {
+        Author expected = new Author("Dan", "Brown");
+        Assertions.assertThat(author).isEqualTo(expected);
+
     }
 
 
