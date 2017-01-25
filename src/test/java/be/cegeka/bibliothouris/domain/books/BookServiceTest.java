9d1a56c5737b47df4564369a94617955e6f96855
Steven Excelmans <steven.excelmans@cegeka.com>
2017-01-25 16:11:08 +0100

    added BookService test - TODO!

---
+++ src/test/java/be/cegeka/bibliothouris/domain/books/BookServiceTest.java
@@ -0,0 +1,15 @@
+package be.cegeka.bibliothouris.domain.books;
+
+/**
+ * Created by stevene on 25/01/2017.
+ */
+/*public class BookServiceTest {
+
+    private Book;
+    private Author;
+
+    public
+
+
+}
+*/
