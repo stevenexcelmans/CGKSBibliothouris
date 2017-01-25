8976edf9ec5f467a44ec833542089160131b81d6
Steven Excelmans <steven.excelmans@cegeka.com>
2017-01-25 10:23:32 +0100

    added Book, BookRepository classes

---
+++ src/main/java/be/cegeka/bibliothouris/domain/books/Book.java
@@ -0,0 +1,47 @@
+package be.cegeka.bibliothouris.domain.books;
+
+public class Book {
+
+    private final long isbn;
+    private final String title;
+    private final Author author;
+
+
+    public Book(long isbn, String title, Author author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (isbn != book.isbn) return false;
+        if (title != null ? !title.equals(book.title) : book.title != null) return false;
+        return author != null ? author.equals(book.author) : book.author == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (isbn ^ (isbn >>> 32));
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/be/cegeka/bibliothouris/domain/books/BookRepository.java
@@ -0,0 +1,18 @@
+package be.cegeka.bibliothouris.domain.books;
+
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named
+public class BookRepository {
+
+    private List<Book> books = new ArrayList<>();
+
+    public List<Book> getBook() {
+        return books;
+    }
+    public void addBook (Book book){
+        books.add(book);
+    }
+}
