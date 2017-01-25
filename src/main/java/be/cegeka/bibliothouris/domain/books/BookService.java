b6a38b12c89bd5b6d9b526285a1afcd0f7746beb
Wouter Bauweraerts <wouter.bauweraerts@cegeka.com>
2017-01-25 11:21:28 +0100

    BookController and additions to service and repo

--- src/main/java/be/cegeka/bibliothouris/domain/books/BookService.java
+++ src/main/java/be/cegeka/bibliothouris/domain/books/BookService.java
@@ -10,7 +10,19 @@ public class BookService {
     @Inject
     private BookRepository bookRepository;
 
+    @Inject
+    private AuthorRepository authorRepository;
+
     public List<Book> getAllBooks(){
         return bookRepository.getAllBooks();
+    }
+
+    public void addBook(long isbn, String title, String firstName, String lastName) {
+        Author author = authorRepository.getAuthor(firstName, lastName);
+        if (author == null){
+            author = authorRepository.addAuthor(firstName, lastName);
+        }
+
+        bookRepository.addBook(new Book(isbn, title, author));
     }
 }
