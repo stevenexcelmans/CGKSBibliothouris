930fd1c96445668f15867dc8dd07d96f03c43b16
Roel Goossens <roelgoossens22@hotmail.com>
2017-01-30 09:59:14 +0100

    Removed obsolete user subdomain

--- src/main/java/be/cegeka/bibliothouris/application/MemberController.java
+++ src/main/java/be/cegeka/bibliothouris/application/MemberController.java
@@ -2,8 +2,6 @@ package be.cegeka.bibliothouris.application;
 
 import be.cegeka.bibliothouris.domain.members.Member;
 import be.cegeka.bibliothouris.domain.members.MemberService;
-import be.cegeka.bibliothouris.domain.users.User;
-import be.cegeka.bibliothouris.domain.users.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,29 +10,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.inject.Inject;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 @RequestMapping("/user")
-public class UserController {
+public class MemberController {
 
     @Inject
-    private UserService userService;
     private MemberService memberService;
 
     @RequestMapping(method = RequestMethod.GET)
     public
     @ResponseBody
-    List<User> getUsers() {
-        return userService.getAllUsers();
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public
-
-    @ResponseBody
-    void addUser(@RequestParam(value = "name", required = true) String name) {
-        userService.addUser(name);
+    List<Member> getMembers() {
+        return memberService.getAllMembers();
     }
 
     @RequestMapping(value = "/addMember", method = RequestMethod.POST)
